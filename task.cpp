0a8618611558d9922699e7bbacda0aafa3a5c44e
Narmadha-prakash <102854832+Narmadha-prakash@users.noreply.github.com>
2022-04-02 14:58:11 +0530

    Add files via upload

---
+++ task.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{   
+   cout<<"size of char:"<<sizeof(char)<<" byte";
+    cout<<"\nsize of int:"<<sizeof(int)<<" bytes";
+     cout<<"\nsize of float:"<<sizeof(float)<<" bytes";
+      cout<<"\nsize of double:"<<sizeof(double)<<" bytes";
+
+}
+
